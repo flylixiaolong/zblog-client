4e7250fcd4a37bb6021988b38e835ed1438d7932
lixiaolong <fly_lxl@foxmail.com>
2018-08-20 23:29:53 +0800

    init porject

---
+++ src/views/Login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="login">
+    <el-row type="flex" class="row-bg" justify="center">
+      <el-col :span="6">
+        <el-form>
+          <el-form-item>
+            <el-input v-model="username" placeholder="用户名"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-input v-model="password" placeholder="密  码"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" class="submmit">登录</el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'login',
+  props: ['username', 'password']
+}
+</script>
+
+<style scoped>
+  .submmit {
+    width: 100%;
+  }
+</style>
