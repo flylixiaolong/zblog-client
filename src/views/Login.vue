ee031507844f141f2462f6b43a3ce6f173cc96bb
lixiaolong <fly_lxl@foxmail.com>
2018-08-25 11:39:22 +0800

    login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="login">
     <el-row type="flex" class="row-bg" justify="center">
-      <el-col :span="6">
+      <el-col :xs="18" :sm="16" :md="6" :lg="5" :xl="4" class="container">
+        <h2 class="">ZBLOG</h2>
         <el-form>
           <el-form-item>
             <el-input v-model="username" placeholder="用户名"></el-input>
@@ -28,7 +29,18 @@ export default {
 </script>
 
 <style scoped>
+  .login {
+    padding: 200px 0;
+  }
+
   .submmit {
     width: 100%;
+  }
+
+  .login .container {
+    background-color: white;
+    padding: 25px;
+    border-radius: 2px;
+    border: 1px solid #eaeefb;
   }
 </style>
